494ca2996647f8703e57504400d76096d967cc22
lnbnatera <lnbnatera@yahoo.com.ph>
2021-07-02 08:59:43 +0800

    feature Add string in app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Added on a feature branch.
         </p>
         <a
           className="App-link"
